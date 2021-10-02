4ce058e4eb4d52628ac457d4bf5ac3b723674f22
brianna groden <briannaggroden@gmail.com>
2021-10-01 23:29:46 -0400

    trying to fix commmit issue

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Hello World');
+console.log('Hello World');
+console.log('update');
+
+console.log('bottom of the file');
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('The DOM is loaded');
+})
